c514ece369cb93bbf313ad641f712791dfb62bcf
saravanan@quilltez.com <saravanan@quilltez.com>
2024-01-27 00:44:11 +0530

    improved

--- app-builder/server.js
+++ app-builder/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
